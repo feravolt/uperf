4a840c4c1f8b51f28d55366cf0e298403ca3669c
FeraVolt <fera.volt@gmail.com>
2021-08-03 10:24:34 +0600

    Update customize.sh

--- magisk/customize.sh
+++ magisk/customize.sh
@@ -2,7 +2,7 @@
 
 $BOOTMODE || abort "! Uperf cannot be installed in recovery."
 ui_print "- Extracting module files"
-unzip -o "$ZIPFILE" -x 'META-INF/*' -d "$MODPATH"
+unzip -o "$ZIPFILE" -x 'META-INF/*' -d "$MODPATH" > /dev/null
 sh "$MODPATH"/setup_uperf.sh
 [ "$?" != "0" ] && abort
 rm "$MODPATH"/setup_uperf.sh
